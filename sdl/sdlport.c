9f17025ab6850844a85fe7ac8fb758c12b1ed0b3
Darknior <darknior@hotmail.com>
2017-10-31 16:20:28 +0100

    Add Command line trick

--- sdl/sdlport.c
+++ sdl/sdlport.c
@@ -10,6 +10,7 @@
 #include "packfile.h"
 #include "video.h"
 #include "menu.h"
+#include "openbor.h"
 
 #define appExit exit
 #undef exit
@@ -48,7 +49,28 @@ int main(int argc, char *argv[]) {
 	dirExists(logsDir, 1);
 	dirExists(screenShotsDir, 1);
 
-	Menu();
+	// Trick to add Command line
+	int romArg = 0;
+    if(argc > 1) {
+        int argl = strlen(argv[1]);
+        /*if(argl > 4 && !memcmp(argv[1], "rom=", 4)) {
+            loadsettings();
+            memcpy(packfile, argv[1] + 4, argl - 4);
+            romArg = 1;
+        }*/
+		loadsettings();
+		memcpy(packfile, argv[1], argl);
+		// Correct the Path
+		if(packfile[argl-1] != '/')
+			strcat(packfile, "/");
+		// Try if the Path exist?
+		if (dirExists(packfile, 0))
+			romArg = 1;
+    }
+    if(!romArg)
+        Menu();
+	
+	//strncpy(packfile, "/home/pi/RetroPie/roms/ports/openbor/Bare Knuckle Vacuum 3.01/", 128);
 	openborMain(argc, argv);
 	borExit(0);
 	return 0;
